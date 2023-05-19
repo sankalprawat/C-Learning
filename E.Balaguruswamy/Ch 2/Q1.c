3f4135c1caf76264f46485c54cbc0739d75fc728
Sankalp Rawat <sankalprawat2000@gmail.com>
2023-05-20 00:32:16 +0530

    This is Ch2

---
+++ E.Balaguruswamy/Ch 2/Q1.c
@@ -0,0 +1,15 @@
+#include <conio.h>
+#include <stdio.h>
+
+int main()
+{
+     int a=5 , b=8 , c=18;
+     int x , y , z;
+    
+    printf("\n The equation of the line is :");
+    printf("%dX + %dY = %d" , &a , &b , &c );
+
+    return 0 ;
+
+
+}
